4a461822a1b4344c6a9357537518539725bc7e6b
Vinicius Iankoski <vine.iankoski@gmail.com>
2025-03-27 20:57:21 -0300

    conhecendo a sintaxe java

---
+++ Main.java
@@ -0,0 +1,43 @@
+/* 
+Aqui começa o nosso estudo sobre a sintaxe do Java:
+- TODO arquivo Java que formos escrever, precisa estar dentro
+de uma classe. Somos obrigados a declarar uma classe em todo
+arquivo que formos escrever.
+- o nome da CLASSE tem que ser igual ao nome do ARQUIVO
+obrigatoriamente. Eu posso sim, declarar outras classes dentro
+do arquivo, mas eu preciso que pelo menos UMA tenha o nome do
+arquivo. Essa classe TEM QUE TER o "public" na frente dela; ou
+seja, a classe que for publica dentro do arquivo tem que ter o
+nome do arquivo.
+*/ 
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * DECLARANDO VARIÁVEIS
+         * Vamos começar falando sobre declarar variáveis no java. Aqui,
+         * temos duas formas de declaração:
+         * 
+         * ## Especificando o tipo da variável
+         * Nessa forma, nós colocamos o tipo, o nome e o valor atribuído a ela:
+         * >> int MinhaIdade = 19;
+         * 
+         * ## Utilizando o var
+         * Não estamos falando do VAR do futebol. Aqui, apenas utilizando a palavra
+         * "var", já podemos criar a variável, sem necessariamente especificar o tipo dela:
+         * >> var meuNome = "Vineolson";
+         * O Java, por si só, vai INFERIR tipo da minha variável, sem que eu especifique.
+         */
+
+         /*
+          * TIPOS PRIMITIVOS: quais são os tipos de variável?
+          *
+          * ## Números inteiros
+          *
+          *
+          *
+          *
+          *
+          */
+
+    }
+}
