3324eda9f9df71310cc28838b7db3be0d121c534
Vinicius Iankoski <vine.iankoski@gmail.com>
2025-03-31 21:18:48 -0300

    mais descobertas sobre o java

--- Main.java
+++ Main.java
@@ -9,7 +9,10 @@ do arquivo, mas eu preciso que pelo menos UMA tenha o nome do
 arquivo. Essa classe TEM QUE TER o "public" na frente dela; ou
 seja, a classe que for publica dentro do arquivo tem que ter o
 nome do arquivo.
-*/ 
+*/
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -37,15 +40,19 @@ public class Main {
           * - short: 16 bits -> -32.768 a 32.767
           * - int: 32 bits -> -2.147.483.648 a 2.147.483.647
           * - long: 64 bits -> -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
-          *
+          * - - long precisa terminar com um "L" no final. Ex: long = 1000000L;
+          * 
           * ## Números Decimais
           *
           * - float: 32 bits -> precisao simples (19.23, 55.0)
+          * - - float precisa terminar com um "f" no final. Ex: float = 10.5f;
           * - double: 64 bits -> precisao dupla (19.9999999, 23.500) para muitos numeros depois da virgula
+          *
+          * Saber essas coisas é bom para entendermos como economizar a memória.
           *
           * ## Textos
           *
-          * - String: representar palavras e frases ("")
+          * - String: representar palavras e frases ("") - na verdade, String é uma classe, não um tipo primitivo.
           * - char: representar um unico caracter ('')
           *
           * ## Verdadeiro ou Falso
@@ -54,5 +61,43 @@ public class Main {
           *
           */
 
+// --> CONDICIONAIS
+    boolean bool = false;
+    String str = "uma frase";
+
+    if (str.equals("uma frasae")) {
+            System.out.println("Verdadeiro!");
+    } else if (str == "uma frase") {
+        System.out.println("Como você adivinhou isso?");
+    }
+    else {
+            System.out.println("Falso!");
+        }
+
+// --> VETORES/ARRAYS
+    int[] grupoDeInteiros = {1, 2, 3};
+    // Desta forma que deixamos, o tamanho do array é fixo baseado no tanto de itens que possui.
+    int[] arrayTeste = new int[5];
+    // Desta forma, nós criamos o array sem necessariamente ter valores, porém com um tamanho {length} definido. 
+    grupoDeInteiros[2] = 5;
+    System.out.println(grupoDeInteiros[2]);
+
+// --> ArrayList
+/*
+ * É uma classe que precisa ser importada de "import java.util.ArrayList;"
+ * Extremamente útil, pois serve para criarmos listas dinâmicas.
+ * - listas que não precisam de um tamanho fixo. Ou seja, podemos
+ * facilmente adicionar e deletar elementos delas.
+ *
+ * Para adicionarmos e deletarmos elementos nele, temos que nos utilizar dos métodos. ".add", ".delete", ".get(0)".
+ * 
+ */
+    ArrayList<String> nomes = new ArrayList<>();
+    nomes.add("jorge"); 
+    nomes.add("robinsos");
+    nomes.remove("jorge");
+    nomes.remove(0);
+    // podemos remover tanto pelo conteudo quanto pelo indice.
+    
     }
 }
